7ce2153f0509d6782c15226c248fcec10a6b898e
Sara-a-r <sarah02_ardito@hotmail.it>
2024-05-02 13:31:37 +0200

    plot features modified

--- code/SRmodel/rms.py
+++ code/SRmodel/rms.py
@@ -30,26 +30,45 @@ rms_FSF = np.flip(np.sqrt(varxx_FSF))
 # compute the response of system (not controlled) when there is the seism
 Out_nc= Tfnc_pl[1:] * ASDseism
 # cumulated RMS
-df = np.diff(freq)
+#df = np.diff(freq) #0.00305176
 varxx_nc = np.cumsum(np.flip(df * (Out_nc[0:-1])**2))
 rms_nc = np.flip(np.sqrt(varxx_nc))
 
-plt.title('SR response to seism', size=11)
-plt.xlabel('f [Hz]')
-plt.ylabel('ASD [m/$\sqrt{Hz}$]')
+#---------------------------------------------------------------------#
+
+plt.title('SR response to seism', size=13)
+plt.xlabel('Frequency [Hz]', size=12)
+plt.ylabel('ASD [m/$\sqrt{Hz}$]', size =12)
 plt.yscale('log')
 plt.xscale('log')
 #plt.ylim(1e-7, 1e6)
-plt.grid(True)
+plt.grid(True, ls='-', alpha=0.3, lw=0.5)  # , which='both',ls='-', alpha=0.3, lw=0.5)
 plt.minorticks_on()
-
 
 
 plt.plot(freq, OutFSF, linestyle='-', linewidth=1, marker='', color='blue', label='control')
 plt.plot(freq[0:-1], rms_FSF, linestyle='--', linewidth=1, marker='', color='Lime', label='rms control')
 plt.plot(freq, Out_nc, linestyle='-', linewidth=1, marker='', color='black', label='no control')
 plt.plot(freq[0:-1], rms_nc, linestyle='--', linewidth=1, marker='', color='red', label='rms no control')
-
 plt.legend()
+
+#---------------------------RMS plot----------------------------#
+fig = plt.figure(figsize=(5, 5))
+plt.title('Mirror RMS displacement', size=13)
+plt.xlabel('Frequency [Hz]', size=12)
+plt.ylabel('RMS [m/$\sqrt{Hz}$]', size =12)
+plt.yscale('log')
+plt.xscale('log')
+#plt.ylim(1e-7, 1e6)
+plt.grid(True, ls='-', alpha=0.3, lw=0.5)  # , which='both',ls='-', alpha=0.3, lw=0.5)
+plt.minorticks_on()
+
+
+plt.plot(freq[0:-1], rms_FSF, linestyle='-', linewidth=1, marker='', color='orange', label='FSF control')
+plt.plot(freq[0:-1], rms_nc, linestyle='-', linewidth=1, marker='', color='steelblue', label='rms no control')
+plt.legend()
+
+
+
 
 plt.show()
